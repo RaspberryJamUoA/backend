e80ca6de2ee85b8bd5a22692cf9f46cddc7e041c
Daniel Li <danielli12357@gmail.com>
2020-08-09 10:15:56 +1200

    Date required

--- src/controllers/eventValidator.ts
+++ src/controllers/eventValidator.ts
@@ -5,7 +5,7 @@ import {IEvent} from '../interfaces/IEvent';
 export const validateBodyEvent = joiValidator(joi.object<IEvent>({
     clubName: joi.string().required(),
     cost: joi.number().required(),
-    dateTime: joi.string().isoDate(),
+    dateTime: joi.string().isoDate().required(),
     description: joi.string().required(),
     eventName: joi.string().required(),
     keywords: joi.array().items(joi.string()),
